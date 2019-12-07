2604f772e220dd89735e6ce281a88aaf361f595a
OstapKoverko <tank302@gmail.com>
2019-12-07 21:07:01 +0200

    Changed style

---
+++ js/stickyContol.js
@@ -0,0 +1,14 @@
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+window.onscroll = function() {
+  stickyContol();
+};
+
+function stickyContol() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
