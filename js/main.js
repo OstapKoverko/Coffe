c76f645e75a83df3cf2c8e44d21f13d04ea96328
OstapKoverko <tank302@gmail.com>
2019-12-11 23:16:43 +0200

    Aded contact form

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('#submit').click(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: 'https://formspree.io/tank302@gmail.com',
+      method: 'POST',
+      data: { email: $('#customer').val(), message: $('#phone').val() },
+      dataType: 'json'
+    }).done(function() {
+      $('#contact').html('<h4>Дякую! Я невдовзі Вам відповім!</h4>');
+    });
+  });
+});
+
+// $('#exampleModal').on('show.bs.modal', function(event) {
+//   // var button = $(event.relatedTarget); // Button that triggered the modal
+//   // var recipient = button.data('whatever'); // Extract info from data-* attributes
+//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//   var modal = $(this);
+//   // modal.find('.modal-title').text('Ваше замовлення:');
+//   // modal.find('.modal-body input').val(recipient);
+// });
