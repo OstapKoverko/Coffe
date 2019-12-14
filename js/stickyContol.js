68bef2a70ebf3eacd0314b348e616b2128843ca7
OstapKoverko <tank302@gmail.com>
2019-12-14 14:10:28 +0200

    Fixed style

--- js/stickyContol.js
+++ js/stickyContol.js
@@ -1,14 +1,14 @@
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
+// var header = document.getElementById("header");
+// var sticky = header.offsetTop;
 
-window.onscroll = function() {
-  stickyContol();
-};
+// window.onscroll = function() {
+//   stickyContol();
+// };
 
-function stickyContol() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
+// function stickyContol() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
